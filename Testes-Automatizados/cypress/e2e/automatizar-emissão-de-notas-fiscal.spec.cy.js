5f5209419e226685f097a9902d9172232f930415
FeJoestar18 <amarofelipe504@gmail.com>
2025-01-22 15:47:48 -0300

    PROJETO ORGANIZADO

---
+++ Testes-Automatizados/cypress/e2e/automatizar-emissão-de-notas-fiscal.spec.cy.js
@@ -0,0 +1,39 @@
+import { faker } from '@faker-js/faker';
+import geradorCNPJ from '../support/geradorRandomCNPJ';
+import generateRandomCPF from '../support/geradorRandomCPF';
+
+
+describe('template spec', () => {
+  it('passes', () => {
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      return false;
+    });
+
+    cy.fixture('ceps.json').then((data) => {
+      const ceps = data.ceps;
+      const cepAleatorio = ceps[Math.floor(Math.random() * ceps.length)];
+
+      const CNPJ = geradorCNPJ();
+      const CPF = generateRandomCPF();
+      const nomeEmpresa = faker.company.name();
+      const nomeCliente = faker.name.fullName();
+      const valorTotal = faker.commerce.price();
+
+      cy.visit('http://localhost/Gerador_de_Notas_Fiscais/Gerador_Nota_Fiscais-PHP/formulario_nota_fiscal.php');
+      
+      cy.get('#nome_empresa').type(nomeEmpresa);
+      cy.get('#cep').type(cepAleatorio).click();  
+      cy.wait(2000);
+      cy.get('#cnpj').type(CNPJ);
+      cy.get('#nome_cliente').type(nomeCliente);
+      cy.get('#cpf').type(CPF);
+      cy.get('#forma_pagamento').should('be.visible').select('Boleto Bancário');
+      cy.get('#valor_total').type(valorTotal);
+
+      cy.get('[type="submit"]').should('be.visible').click();
+      cy.visit('http://localhost/Gerador_de_Notas_Fiscais/Gerador_Nota_Fiscais-PHP/gerador_nota_fiscal.php');   
+
+      cy.visit('http://localhost/Gerador_de_Notas_Fiscais/Gerador_Nota_Fiscais-PHP/formulario_nota_fiscal.php');
+    });
+  });
+});
