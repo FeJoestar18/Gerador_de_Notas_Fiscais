674244a43741ed966f27f5fe06f042e7743c799f
FeJoestar18 <amarofelipe504@gmail.com>
2025-01-23 11:25:17 -0300

    MOSTRAR CAMPOS REMOVIDOS

--- Gerador_Nota_Fiscais-PHP/formulario_nota_fiscal.php
+++ Gerador_Nota_Fiscais-PHP/formulario_nota_fiscal.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <script src="js/BuscarEndereco.js"></script>
-    <script src="js/MostrarCampos.js"></script>
     <script src="js/BuscarEnderecoCliente.js"></script>
     <title>Gerador de Notas Fiscais</title>
 </head>
