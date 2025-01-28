e6bb35443ac0ca9940324d1fa3fe9508a677d930
FeJoestar18 <amarofelipe504@gmail.com>
2025-01-28 15:19:26 -0300

    Mensagem explicativa sobre as mudanças

--- Gerador_Nota_Fiscais-PHP/teste.php
+++ Gerador_Nota_Fiscais-PHP/teste.php
@@ -1,10 +1,354 @@
+<style type="text/css">
+    @media print {
+    @page {
+        size: 180mm 250mm; 
+        margin-left: 10mm;
+        margin-right: 10mm;
+        margin-top: 5mm;
+        margin-bottom: 5mm;
+    }
+}
+
+
+        footer {
+            page-break-after: always;
+        }
+    }
+
+    * {
+        margin: 0;
+    }
+
+    .ui-widget-content {
+        border: none !important;
+    }
+
+    .nfe-square {
+        margin: 0 auto 2cm;
+        box-sizing: border-box;
+        width: 2cm;
+        height: 1cm;
+        border: 1px solid #000;
+    }
+
+    .nfeArea.page {
+        width: 18cm;
+        position: relative;
+        font-family: "Times New Roman", serif;
+        color: #000;
+        margin: 0 auto;
+        overflow: hidden;
+    }
+
+    .nfeArea .font-12 {
+        font-size: 12pt;
+    }
+
+    .nfeArea .font-8 {
+        font-size: 8pt;
+    }
+
+    .nfeArea .bold {
+        font-weight: bold;
+    }
+    /* == TABELA == */
+    .nfeArea .area-name {
+        font-family: "Times New Roman", serif;
+        color: #000;
+        font-weight: bold;
+        margin: 5px 0 0;
+        font-size: 6pt;
+        text-transform: uppercase;
+    }
+
+    .nfeArea .txt-upper {
+        text-transform: uppercase;
+    }
+
+    .nfeArea .txt-center {
+        text-align: center;
+    }
+
+    .nfeArea .txt-right {
+        text-align: right;
+    }
+
+    .nfeArea .nf-label {
+        text-transform: uppercase;
+        margin-bottom: 3px;
+        display: block;
+    }
+
+        .nfeArea .nf-label.label-small {
+            letter-spacing: -0.5px;
+            font-size: 4pt;
+        }
+
+    .nfeArea .info {
+        font-weight: bold;
+        font-size: 8pt;
+        display: block;
+        line-height: 1em;
+    }
+
+    .nfeArea table {
+        font-family: "Times New Roman", serif;
+        color: #000;
+        font-size: 5pt;
+        border-collapse: collapse;
+        width: 100%;
+        border-color: #000;
+        border-radius: 5px;
+    }
+
+    .nfeArea .no-top {
+        margin-top: -1px;
+    }
+
+    .nfeArea .mt-table {
+        margin-top: 3px;
+    }
+
+    .nfeArea .valign-middle {
+        vertical-align: middle;
+    }
+
+    .nfeArea td {
+        vertical-align: top;
+        box-sizing: border-box;
+        overflow: hidden;
+        border-color: #000;
+        padding: 1px;
+        height: 5mm;
+    }
+
+    .nfeArea .tserie {
+        width: 32.2mm;
+        vertical-align: middle;
+        font-size: 8pt;
+        font-weight: bold;
+    }
+
+        .nfeArea .tserie span {
+            display: block;
+        }
+
+        .nfeArea .tserie h3 {
+            display: inline-block;
+        }
+
+    .nfeArea .entradaSaida .legenda {
+        text-align: left;
+        margin-left: 2mm;
+        display: block;
+    }
+
+        .nfeArea .entradaSaida .legenda span {
+            display: block;
+        }
+
+    .nfeArea .entradaSaida .identificacao {
+        float: right;
+        margin-right: 2mm;
+        border: 1px solid black;
+        width: 5mm;
+        height: 5mm;
+        text-align: center;
+        padding-top: 0;
+        line-height: 5mm;
+    }
+
+    .nfeArea .hr-dashed {
+        border: none;
+        border-top: 1px dashed #444;
+        margin: 5px 0;
+    }
+
+    .nfeArea .client_logo {
+        height: 27.5mm;
+        width: 28mm;
+        margin: 0.5mm;
+    }
+
+    .nfeArea .title {
+        font-size: 10pt;
+        margin-bottom: 2mm;
+    }
+
+    .nfeArea .txtc {
+        text-align: center;
+    }
+
+    .nfeArea .pd-0 {
+        padding: 0;
+    }
+
+    .nfeArea .mb2 {
+        margin-bottom: 2mm;
+    }
+
+    .nfeArea table table {
+        margin: -1pt;
+        width: 100.5%;
+    }
+
+    .nfeArea .wrapper-table {
+        margin-bottom: 2pt;
+    }
+
+        .nfeArea .wrapper-table table {
+            margin-bottom: 0;
+        }
+
+            .nfeArea .wrapper-table table + table {
+                margin-top: -1px;
+            }
+
+    .nfeArea .boxImposto {
+        table-layout: fixed;
+    }
+
+        .nfeArea .boxImposto td {
+            width: 11.11%;
+        }
+
+        .nfeArea .boxImposto .nf-label {
+            font-size: 5pt;
+        }
+
+        .nfeArea .boxImposto .info {
+            text-align: right;
+        }
+
+    .nfeArea .wrapper-border {
+        border: 1px solid #000;
+        border-width: 0 1px 1px;
+        height: 75.7mm;
+    }
+
+        .nfeArea .wrapper-border table {
+            margin: 0 -1px;
+            width: 100.4%;
+        }
+
+    .nfeArea .content-spacer {
+        display: block;
+        height: 10px;
+    }
+
+    .nfeArea .titles th {
+        padding: 3px 0;
+    }
+
+    .nfeArea .listProdutoServico td {
+        padding: 0;
+    }
+
+    .nfeArea .codigo {
+        display: block;
+        text-align: center;
+        margin-top: 5px;
+    }
+
+    .nfeArea .boxProdutoServico tr td:first-child {
+        border-left: none;
+    }
+
+    .nfeArea .boxProdutoServico td {
+        font-size: 6pt;
+        height: auto;
+    }
+
+    .nfeArea .boxFatura span {
+        display: block;
+    }
+
+    .nfeArea .boxFatura td {
+        border: 1px solid #000;
+    }
+
+    .nfeArea .freteConta .border {
+        width: 5mm;
+        height: 5mm;
+        float: right;
+        text-align: center;
+        line-height: 5mm;
+        border: 1px solid black;
+    }
+
+    .nfeArea .freteConta .info {
+        line-height: 5mm;
+    }
+
+    .page .boxFields td p {
+        font-family: "Times New Roman", serif;
+        font-size: 5pt;
+        line-height: 1.2em;
+        color: #000;
+    }
+
+    .nfeArea .imgCanceled {
+        position: absolute;
+        top: 75mm;
+        left: 30mm;
+        z-index: 3;
+        opacity: 0.8;
+        display: none;
+    }
+
+    .nfeArea.invoiceCanceled .imgCanceled {
+        display: block;
+    }
+
+    .nfeArea .imgNull {
+        position: absolute;
+        top: 75mm;
+        left: 20mm;
+        z-index: 3;
+        opacity: 0.8;
+        display: none;
+    }
+
+    .nfeArea.invoiceNull .imgNull {
+        display: block;
+    }
+
+    .nfeArea.invoiceCancelNull .imgCanceled {
+        top: 100mm;
+        left: 35mm;
+        display: block;
+    }
+
+    .nfeArea.invoiceCancelNull .imgNull {
+        top: 65mm;
+        left: 15mm;
+        display: block;
+    }
+
+    .nfeArea .page-break {
+        page-break-before: always;
+    }
+
+    .nfeArea .block {
+        display: block;
+    }
+
+    .label-mktup {
+        font-family: Arial !important;
+        font-size: 8px !important;
+        padding-top: 8px !important;
+    }
+</style>
+<html>
+<body>
+<table>
 </td>
                     <td rowspan="3" class="txtc txt-upper" style="width: 34mm; height: 29.5mm;">
                         <h3 class="title">Danfe</h3>
                         <p class="mb2">Documento auxiliar da Nota Fiscal Eletrônica </p>
                         <p class="entradaSaida mb2">
                             <span class="identificacao">
-                                <span>'.ds_code_operation_type.'</span>
+                                <span></span>
                             </span>
                             <span class="legenda">
                                 <span>0 - Entrada</span>
@@ -22,21 +366,21 @@
                             </span>
                             <span class="block">
                                 <span>Página</span>
-                                <span>'.actual_page.'</span>
+                                <span></span>
                                 <span>de</span>
-                                <span>'.total_pages.'</span>
+                                <span></span>
                             </span>
                         </p>
                     </td>
                     <td class="txt-upper" style="width: 85mm;">
                         <span class="nf-label">Controle do Fisco</span>
-                        <span class="codigo">{BarCode}</span>
+                        <span class="codigo"></span>
                     </td>
                 </tr>
                 <tr>
                     <td>
                         <span class="nf-label">CHAVE DE ACESSO</span>
-                        <span class="bold block txt-center info">'.ds_danfe.'</span>
+                        <span class="bold block txt-center info">'. $key .'</span>
                     </td>
                 </tr>
                 <tr>
@@ -55,8 +399,8 @@
                         <span class="info">'.$natureza_operacao.'</span>
                     </td>
                     <td style="width: 84.7mm;">
-                        <span class="nf-label">'.protocol_label.'</span>
-                        <span class="info">'.ds_protocol.'</span>
+                        <span class="nf-label"></span>
+                        <span class="info"></span>
                     </td>
                 </tr>
             </tbody>
@@ -72,7 +416,7 @@
                     </td>
                     <td style="width: 67.5mm;">
                         <span class="nf-label">INSCRIÇÃO ESTADUAL DO SUBST. TRIB.</span>
-                        <span class="info">'.nl_company_ie_st.'</span>
+                        <span class="info"></span>
                     </td>
                     <td style="width: 64.3mm">
                         <span class="nf-label">CNPJ</span>
@@ -153,7 +497,7 @@
                                     </td>
                                     <td style="width: 51mm">
                                         <span class="nf-label">INSCRIÇÃO ESTADUAL</span>
-                                        <span class="info">'.ds_client_ie.'</span>
+                                        <span class="info"></span>
                                     </td>
                                 </tr>
                             </tbody>
@@ -161,7 +505,7 @@
                     </td>
                     <td>
                         <span class="nf-label">HORA ENTR./SAÍDA</span>
-                        <span id="info">'.hr_input_output.'</span>
+                        <span id="info"></span>
                     </td>
                 </tr>
             </tbody>
@@ -169,10 +513,10 @@
         <!-- Fatura -->
         <div class="boxFatura">
             <p class="area-name">Fatura</p>
-            '.duplicates.'
+           
         </div>
  
-        <!-- Calculo do Imposto -->
+       
         <p class="area-name">Calculo do imposto</p>
         <div class="wrapper-table">
             <table cellpadding="0" cellspacing="0" border="1" class="boxImposto">
@@ -180,19 +524,19 @@
                     <tr>
                         <td>
                             <span class="nf-label label-small">BASE DE CÁLC. DO ICMS</span>
-                            <span class="info">'.tot_bc_icms.'</span>
+                            <span class="info"></span>
                         </td>
                         <td>
                             <span class="nf-label">VALOR DO ICMS</span>
-                            <span class="info">'.tot_icms.'</span>
+                            <span class="info"></span>
                         </td>
                         <td>
                             <span class="nf-label label-small" style="font-size: 4pt;">BASE DE CÁLC. DO ICMS ST</span>
-                            <span class="info">'.tot_bc_icms_st.'</span>
+                            <span class="info"></span>
                         </td>
                         <td>
                             <span class="nf-label">VALOR DO ICMS ST</span>
-                            <span class="info">'.tot_icms_st.'</span>
+                            <span class="info"></span>
                         </td>
                         <td>
                             <span class="nf-label label-small">V. IMP. IMPORTAÇÃO</span>
@@ -204,7 +548,7 @@
                         </td>
                         <td>
                             <span class="nf-label">VALOR DO FCP</span>
-                            <span class="info">'.tot_icms_fcp.'</span>
+                            <span class="info"></span>
                         </td>
                         <td>
                             <span class="nf-label">VALOR DO PIS</span>
@@ -218,23 +562,23 @@
                     <tr>
                         <td>
                             <span class="nf-label">VALOR DO FRETE</span>
-                            <span class="info">'.vl_shipping.'</span>
+                            <span class="info"></span>
                         </td>
                         <td>
                             <span class="nf-label">VALOR DO SEGURO</span>
-                            <span class="info">'.vl_insurance.'</span>
+                            <span class="info"></span>
                         </td>
                         <td>
                             <span class="nf-label">DESCONTO</span>
-                            <span class="info">'.vl_discount.'</span>
+                            <span class="info"></span>
                         </td>
                         <td>
                             <span class="nf-label">OUTRAS DESP.</span>
-                            <span class="info">'.vl_other_expense.'</span>
+                            <span class="info"></span>
                         </td>
                         <td>
                             <span class="nf-label">VALOR DO IPI</span>
-                            <span class="info">'.tot_total_ipi_tax.'</span>
+                            <span class="info"></span>
                         </td>
                         <td>
                             <span class="nf-label">V. ICMS UF DEST.</span>
@@ -242,7 +586,7 @@
                         </td>
                         <td>
                             <span class="nf-label label-small">V. APROX. DO TRIBUTO</span>
-                            <span class="info">{ApproximateTax}</span>
+                            <span class="info"></span>
                         </td>
                         <td>
                             <span class="nf-label label-small">VALOR DA CONFINS</span>
@@ -280,7 +624,7 @@
                     </tr>
                 </thead>
                 <tbody>
-                    '.items.'
+                    
                 </tbody>
             </table>
         </div>
@@ -292,7 +636,7 @@
                 <tr>
                     <td class="field inscrMunicipal">
                         <span class="nf-label">INSCRIÇÃO MUNICIPAL</span>
-                        <span class="info txt-center">'.ds_company_im.'</span>
+                        <span class="info txt-center">'. $inscricao_municipal .'</span>
                     </td>
                     <td class="field valorTotal">
                         <span class="nf-label">VALOR TOTAL DOS SERVIÇOS</span>
@@ -304,7 +648,7 @@
                     </td>
                     <td class="field valorIssqn">
                         <span class="nf-label">VALOR DO ISSQN</span>
-                        <span class="info txt-right">'.tot_issqn.'</span>
+                        <span class="info txt-right"></span>
                     </td>
                 </tr>
             </tbody>
@@ -339,3 +683,6 @@
     </div>
     
 </div>
+</table>
+</body>
+</html>
